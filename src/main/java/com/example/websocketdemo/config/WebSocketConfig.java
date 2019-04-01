c27536141d09c1202f4a1b18046a344809d24ad9
Ferb_jailer <kushrao16@gmail.com>
2019-03-31 21:21:59 -0300

    chat project  commit

---
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.example.websocketdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+
+@Configuration
+@EnableWebSocketMessageBroker 
+// this above tag is use to enable webSocket server
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+    
+    //  In this first method we register a websocket EndPoint that the client will use to connect 
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");   // This enables simple in memory Broker that help to communicate 
+
+
+        //   Use this for enabling a Full featured broker like RabbitMQ
+
+        /*
+        registry.enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+        */
+    }
+}
